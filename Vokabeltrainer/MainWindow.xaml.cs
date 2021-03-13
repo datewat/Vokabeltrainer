2a1ae8504fff2de6e82c4ce7db142ac83f88e357
datewat <thomaszimmer524@gmail.com>
2021-03-13 16:35:15 +0100

    Projekt hinzugefügt
    Das gesamte Projekt hinzugefügt. Später werden Vokabeln hinzugefügt.

---
+++ Vokabeltrainer/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace Vokabeltrainer
+{
+
+    public partial class MainWindow : Window
+    {
+
+        public static string[] v2 = File.ReadAllLines(@"2.txt");
+        public static string[] v1 = File.ReadAllLines(@"1.txt");
+        public static Random random;
+        public static int rnd;
+        public static int score = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowScore.Content = $"Score: {score}";
+        }
+
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {
+            random = new Random();
+            rnd = random.Next(v2.Length);
+            Vocabulary.Content = v2[rnd];
+        }
+
+        private void Input_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputPanel.Text == v1[rnd])
+            {
+                Generate.Content = "Skip";
+                score += 1;
+                ShowScore.Content = $"Score: {score}";
+                rnd = random.Next(v2.Length);
+                Vocabulary.Content = v2[rnd];
+                gotWrong.Content = "";
+                InputPanel.Text = "";
+            }
+            else
+            {
+                gotWrong.Content = "Try again!";
+            }
+        }
+    }
+}
